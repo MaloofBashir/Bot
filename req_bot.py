3a04a299ad4eb4ab47bbb0dc67fe87c6760e99a1
Maloof <44017606+MaloofBashir@users.noreply.github.com>
2025-06-06 23:02:37 +0530

    Update req_bot.py

--- req_bot.py
+++ req_bot.py
@@ -1,12 +1,11 @@
 import requests
 import time
 
+
 URL = "https://collegedext.onrender.com/"
 
-while True:
-    try:
-        response = requests.get(URL)
-        print(f"Status Code: {response.status_code}")
-    except Exception as e:
-        print(f"Error: {e}")
-    time.sleep(600)  # Wait for 10 minutes
+try:
+    response = requests.get(URL)
+    print(f"Status Code: {response.status_code}")
+except Exception as e:
+    print(f"Error: {e}")
