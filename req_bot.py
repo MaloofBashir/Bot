781f500942cf4d4407c1d3fae024109cf4430a64
Maloof <44017606+MaloofBashir@users.noreply.github.com>
2025-06-07 19:09:19 +0530

    Update req_bot.py

--- req_bot.py
+++ req_bot.py
@@ -1,11 +1,15 @@
 import requests
 import time
-
+import datetime
 
 URL = "https://collegedext.onrender.com/"
+print(f"Running at {datetime.datetime.utcnow()} UTC")
 
 try:
     response = requests.get(URL)
     print(f"Status Code: {response.status_code}")
 except Exception as e:
     print(f"Error: {e}")
+
+# Sleep to simulate real work (GitHub might skip jobs if they’re too short)
+time.sleep(60) 
